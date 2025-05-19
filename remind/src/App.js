d1c82cf412b9d467feaf0438956e5b9afd3bedf1
yena <2371429@hansung.ac.kr>
2025-05-19 17:40:37 +0900

    ♻️ 메인 홈 화면 페이지 라우팅 방식 전환

--- remind/src/App.js
+++ remind/src/App.js
@@ -1,21 +1,11 @@
-import Header from "./components/home/header/Header";
-import Intro from "./components/home/intro/Intro";
-import MemoryRecord from "./components/home/memoryrecord/MemoryRecord";
-import PlanArchive from "./components/home/planarchive/PlanArchive";
-import Rimi from "./components/home/rimi/Rimi";
-import TodayPlan from "./components/home/todayplan/TodayPlan";
+import MainHome from "./pages/MainHome";
 import GlobalStyle from "./styles/globalStyles";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <Intro />
-      <PlanArchive />
-      <TodayPlan />
-      <MemoryRecord />
-      <Rimi />
+      <MainHome />
     </>
   );
 }
