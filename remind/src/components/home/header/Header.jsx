afa3a9eb28f49a841068399c1d770ef8560c3b6c
yena <2371429@hansung.ac.kr>
2025-05-19 18:12:44 +0900

    ✨ Header 로그인 연동 대비 상태 구성

--- remind/src/components/home/header/Header.jsx
+++ remind/src/components/home/header/Header.jsx
@@ -1,21 +1,23 @@
 import * as S from "./Header.style";
 import LOGO from "../../../assets/home/remind.svg";
+import { useState } from "react";
 
 export default function Header() {
+    const [userName, setUserName] = useState("000");
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+
     return (
         <S.HeaderLayout>
             <S.Home>
                 <S.Image src = { LOGO } />
             </S.Home>
+
             <S.User>
-                안녕하세요 000 님  
+                안녕하세요 { isLoggedIn && userName ? `${ userName } 님` : "" }
             </S.User>
-            <S.Account>
-                회원가입
-            </S.Account>
-            <S.Account>
-                로그인
-            </S.Account>
+
+            <S.Account>회원가입</S.Account>
+            <S.Account>로그인</S.Account>
         </S.HeaderLayout>
     )
 }
