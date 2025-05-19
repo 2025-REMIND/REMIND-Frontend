41d8dd97e4804f8f815564083d1f1be2833f8694
yena <2371429@hansung.ac.kr>
2025-05-19 09:12:33 +0900

    :lipstick: globalStyles 추가

--- remind/src/App.js
+++ remind/src/App.js
@@ -1,8 +1,8 @@
+import GlobalStyle from "./styles/globalStyles";
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <GlobalStyle />
   );
 }
 
---
+++ remind/src/styles/globalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        font-family: 'Pretendard';
+    }
+
+    body {
+        background-color: #FFFAF8;
+    }
+`;
+
+export default GlobalStyle;
