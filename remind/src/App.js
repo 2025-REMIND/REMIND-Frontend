f9aed6e9414fdf39275ee56699c715049e8c86fb
yena <2371429@hansung.ac.kr>
2025-05-20 09:52:54 +0900

    ✨ 메인 홈에서 기억 기록 페이지 이동 기능 추가

--- remind/src/App.js
+++ remind/src/App.js
@@ -1,12 +1,17 @@
 import MainHomePage from "./pages/MainHomePage";
+import MemoryRecordPage from "./pages/MemoryRecordPage";
 import GlobalStyle from "./styles/globalStyles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyle />
-      <MainHomePage />
-    </>
+      <Routes>
+        <Route path = "/" element = { <MainHomePage /> } />
+        <Route path = "/memoryrecord" element = { <MemoryRecordPage /> } />
+      </Routes>
+    </Router>
   );
 }
 
