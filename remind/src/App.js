8fa50866e6ce9528f9820fbd4f35e405191be9f6
yena <2371429@hansung.ac.kr>
2025-05-21 13:18:06 +0900

    🐛 App.js 충돌 수정

--- remind/src/App.js
+++ remind/src/App.js
@@ -1,18 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {LoginForm} from './components/LoginForm/LoginForm';
+import {RegisterForm} from './components/Regitster/RegisterForm'; // 예시
 import MainHomePage from "./pages/MainHomePage";
 import MemoryRecordPage from "./pages/MemoryRecordPage";
 import GlobalStyle from "./styles/globalStyles";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path = "/" element = { <MainHomePage /> } />
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
         <Route path = "/home" element = { <MainHomePage /> } />
         <Route path = "/memoryrecord" element = { <MemoryRecordPage /> } />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
