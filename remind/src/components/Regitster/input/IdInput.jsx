4fd9ca14105f287c7aa1357f16ce83f6481ed465
Parksh <2171164@hansung.ac.kr>
2025-05-21 13:26:16 +0900

    ♻️ 회원가입 컴포넌트 재구성

---
+++ remind/src/components/Regitster/input/IdInput.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import * as S from '../RegisterForm.style';
+
+const IdInput = ({ id, onChange, error }) => (
+  <S.Row>
+    <S.Label>아이디 <span>*</span></S.Label>
+    <S.InputRow>
+      <S.InputWithButton>
+        <S.Input
+          placeholder="아이디를 입력해주세요"
+          value={id}
+          onChange={onChange}
+          $isError={!!error}
+        />
+        <S.CheckButton>아이디 중복 확인</S.CheckButton>
+      </S.InputWithButton>
+      <S.Guideline style={{ color: error ? 'red' : '#999' }}>
+        {error || '영문 / 숫자 혼용하여 4자~10자 이내로 작성해주세요'}
+      </S.Guideline>
+    </S.InputRow>
+  </S.Row>
+);
+
+export default IdInput;
