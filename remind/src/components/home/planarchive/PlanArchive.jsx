2698b251cfe35b19881a873262a5be576c810fa1
yena <2371429@hansung.ac.kr>
2025-05-25 03:41:16 +0900

    📝 홈 화면 일정 보관함 부분 수정

--- remind/src/components/home/planarchive/PlanArchive.jsx
+++ remind/src/components/home/planarchive/PlanArchive.jsx
@@ -1,80 +1,19 @@
 import * as S from "./PlanArchive.style";
-import DATE from "../../../assets/home/planarchive/date.svg";
-import DRAMA from "../../../assets/home/planarchive/drama.svg";
-import LOVE from "../../../assets/home/planarchive/love.svg";
-import SONG from "../../../assets/home/planarchive/song.svg";
-import { useState } from "react";
 
 export default function PlanArchive() {
-    const [archiveList] = useState([
-        {
-            id: 1,
-            icon: DATE,
-            date: "2025. 03. 13.",
-            name: "'RIMI'가 추천한 우리 데이트",
-            comment: "날씨도 좋은데 한강 데이트 가서 봄바람 같이 쐐기",
-            status: "ing",
-        },
-        {
-            id: 2,
-            icon: LOVE,
-            date: "2025. 03. 13.",
-            name: "영화관에서 본 그날그",
-            comment: "처음으로 같이 봤던\n영화 기억나?",
-            status: "x",
-        },
-        {
-            id: 3,
-            icon: SONG,
-            date: "2025. 03. 13.",
-            name: "노래방에서 부른 노래",
-            comment: "그 시절 즐겨 듣던 노래들\n다시 불러보기",
-            status: "x",
-        },
-        {
-            id: 4,
-            icon: DRAMA,
-            date: "2025. 03. 13.",
-            name: "좋아했던 드라마",
-            comment: "세 번째 데이트 때 본 그 드라마",
-            status: "detail",
-        },
-    ]);
-
-    const planStatus = (status) => {
-        switch (status) {
-            case "ing":
-                return "진행중";
-            case "x":
-                return "미완료";
-            case "detail":
-                return "자세히";
-        }
-    }
-
     return (
-        <S.ArchiveLayout>
-            <S.Title>일정 보관함</S.Title>
-            <S.SubTitle>추억이 담긴 일정들을 한 곳에, 우리의 시간, 고스란히 모아둘게요.</S.SubTitle>
-            <S.Detail>
-                <S.DetailText>자세히 보러 가기</S.DetailText>
-                <S.IconDetail>→</S.IconDetail>
-            </S.Detail>
-            <S.ArchiveWrapper>
-                { archiveList.map((item) => (
-                    <S.ArchiveBox  key = { item.id } status = { item.status }>
-                        <S.Archive>
-                            <S.Info>
-                                <S.Icon src = { item.icon } />
-                                <S.Date>2025. 03. 13.</S.Date>
-                            </S.Info>
-                            <S.Name status = { item.status }>{ item.name }</S.Name>
-                            <S.Comment status = { item.status }>{ item.comment }</S.Comment>
-                        </S.Archive>
-                        <S.Status>{ planStatus(item.status) }</S.Status>
-                    </S.ArchiveBox>
-                ))}
-            </S.ArchiveWrapper>
-    </S.ArchiveLayout>
+        <S.PlanArchiveLayout>
+            <S.Text>
+                <S.Title>일정 보관함</S.Title>
+                <S.Comment>
+                    {`우리만의 소중한 순간들이 흐트러지지 않도록,
+                    기억 속 일정들을 한데 모아 따뜻하게 보관해 둘 수 있는 공간이에요.
+                    함께한 날들의 설렘과 웃음, 때로는 그리움까지ㅡ
+                    모든 감정의 파편들을 다시 꺼내어 볼 수 있도록, RE:MIND가 당신의 시간에 감성을 입혀 드릴게요.
+                    그날의 풍경, 들었던 노래, 느꼈던 감정들까지 고스란히 담아, 우리의 이야기를 다시 시작해 볼 수 있어요.`}
+                </S.Comment>
+            </S.Text>
+            <S.PlanArchiveBox>일정 보관함 바로가기</S.PlanArchiveBox>       
+        </S.PlanArchiveLayout>
     )
 }
--- remind/src/components/home/planarchive/PlanArchive.style.js
+++ remind/src/components/home/planarchive/PlanArchive.style.js
@@ -1,130 +1,51 @@
 import styled from "styled-components";
 
-export const ArchiveLayout = styled.div`
+export const PlanArchiveLayout = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
-    height: 80.98vw;
+    width: 100vw;
+    height: 44.32vw;
+    gap: 6.25vw;
 `
 
-export const Title = styled.div`
+export const Text = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2.06vw;
+`
+
+export const Title = styled.p`
+    display: flex;
+    justify-content: center;
     font-weight: 700;
     font-size: 3.38vw;
-    margin-top: 6.25vw;
-    margin-bottom: 0.67vw;
     color: #d7749b;
+    margin: 0;
 `
 
-export const SubTitle = styled.div`
-    font-weight: 400;
+export const Comment = styled.p`
+    display: flex;
+    text-align: center;
+    color: #9D8C84;
     font-size: 1.56vw;
-    color: #9D8C84;
-`
-
-export const Detail = styled.div`
-    display: flex;
-    align-self: flex-end;
-    margin-top: 2.34vw;
-    margin-right: 17.05vw;
-    gap: 0.13vw;
-`
-
-export const DetailText = styled.div`
     font-weight: 400;
-    font-size: 1.30vw;
-    color: #9D8C84;
-    border-bottom: 0.1vw solid #9D8C84;
-`
-
-export const IconDetail = styled.div`
-    font-weight: 400;
-    font-size: 1.30vw;
-    color: #9D8C84;
-`
-
-export const ArchiveWrapper = styled.div`
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 2.60vw
-`
-
-export const ArchiveBox = styled.div`
-    position: relative;
-    width: 31.25vw;
-    height: 26.04vw;
-    margin-top: 1.04vw;
-    border-radius: 1.56vw;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-
-    background-color: ${({ status }) => {
-        if (status === "ing" || status === "detail") return "#F6BFCF";
-        else if (status === "x") return "#FFFAF8";
-    }};
-
-    border: ${({ status }) => {
-        if (status === "x") return "0.05vw solid #D7749B";
-    }};
-`
-
-export const Archive = styled.div`
-    margin-left: 2.08vw;
-    margin-right: 2.08vw;
-`
-
-export const Info = styled.div`
-    display: flex;
-    margin-top: 2.39vw;
-    margin-bottom: 1.56vw;
-    gap: 1.14vw;
-`
-
-export const Icon = styled.img`
-    width: 2.76vw;
-    height: 2.76vw;
-`
-
-export const Date = styled.div`
-    display: flex;
-    align-items: center;
-    color: #d7749b;
-`
-
-export const Name = styled.div`
-    font-weight: 700;
-    font-size: 2.60vw;
-    color: ${({ status }) => {
-        if (status === "ing" || status === "detail") return "#FFFAF8";
-        else if (status === "x") return "#D7749B";
-    }};
-    margin-bottom: 1.25vw;
-    white-space: nowrap;
-    overflow: hidden; 
-    text-overflow: ellipsis; 
-`
-
-export const Comment = styled.div`
-    font-weight: 400;
-    font-size: 2.08vw;
-    color: ${({ status }) => {
-        if (status === "ing" || status === "detail") return "#FFFAF8";
-        else if (status === "x") return "#D7749B";
-    }};
     line-height: 140%;
     white-space: pre-line;
+    margin: 0;
 `
 
-export const Status = styled.div`
-    position: absolute;
-    bottom: 2.08vw;
-    right: 2.08vw;
+export const PlanArchiveBox = styled.div`
     display: flex;
-    align-items: center;
+    flex-direction: column;
     justify-content: center;
-    background-color: #FFFAF8;
+    text-align: center;
+    width: 26.04vw;
+    height: 7.81vw;
     border: 0.05vw solid #D7749B;
-    border-radius: 0.75vw;
+    border-radius: 1.56vw;
     color: #D7749B;
-    font-size: 1.56vw;
-    width: 8.85vw;
-    height: 4.16vw;
+    font-size: 2.08vw;
+    font-weight: 600;
 `
