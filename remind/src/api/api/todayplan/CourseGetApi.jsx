232a34a5c6220250c847eb7bdd7624f3d5aa6ecf
yena <2371429@hansung.ac.kr>
2025-05-28 00:57:53 +0900

    ✨ 코스 추천 팝업 API 연동동

---
+++ remind/src/api/api/todayplan/CourseGetApi.jsx
@@ -0,0 +1,15 @@
+import defaultInstance from "../../utils/instance";
+
+const CourseGetApi = async (memberId, courseId) => {
+    try {
+        const response = await defaultInstance.get(`/mission/${memberId}/${courseId}`);
+
+        if (response.data.httpStatus === 200) 
+            return response.data.data;
+        else console.log("실패", response.data.message);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export default CourseGetApi;
